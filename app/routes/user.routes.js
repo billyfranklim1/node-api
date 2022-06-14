203929498af969ecbb60704d6350fe008400bd3f
billyfranklim <billyfranklim@gmail.com>
2022-06-14 20:01:36 -0300

    news endpoints

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -6,15 +6,10 @@ module.exports = function(app) {
     res.header("Access-Control-Allow-Headers","Origin, Content-Type, Accept");
     next();
   });
-  // app.get("/api/test/all", controller.allAccess);
   app.get("/api/user/:id", [authJwt.verifyToken], controller.getUser);
-  // app.get("/api/test/mod",[authJwt.verifyToken, authJwt.isModerator],controller.moderatorBoard);
-  // app.get("/api/test/admin",[authJwt.verifyToken, authJwt.isAdmin],controller.adminBoard);
-  // app.get('/user/:id', controller.getUser);
-  app.get('/api/user',controller.getUsers);
-  // app.post('/api/user',controller.createUser);
-  // app.put('/api/user',controller.updateUser);
-  // app.delete('/api/user/:id',controller.delete);
-  // app.post('/login', controller.login);
+  app.get('/api/user',[authJwt.verifyToken],controller.getUsers);
+  app.post('/api/user',controller.createUser);
+  app.put('/api/user',controller.updateUser);
+  app.delete('/api/user/:id',controller.delete);
 };
 
